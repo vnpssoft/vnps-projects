c96c8057782b375e5ae3e2bd9e6bd2a9689491c5
sankarnatarajan <n.sankar1973@gmail.com>
2016-01-06 21:31:33 -0800

    Moved certain classes to Dao to avoid circular references.

--- usermanagement/usermanagement-dao/src/main/java/com/vnps/dao/usermanagement/UsermanagementDao.java
+++ usermanagement/usermanagement-dao/src/main/java/com/vnps/dao/usermanagement/UsermanagementDao.java
@@ -2,6 +2,7 @@ package com.vnps.dao.usermanagement;
 
 import com.vnps.core.jpa.dao.AbstractDao;
 import com.vnps.core.jpa.dao.DaoException;
+import com.vnps.domain.usermanagement.User;
 
 public class UsermanagementDao extends AbstractDao{
 
@@ -9,5 +10,10 @@ public class UsermanagementDao extends AbstractDao{
 		super(puName);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void addUser(User u) throws DaoException{
+		save(u);
+	}
+	
 	
 }
